02404fe524a0b650c993c6f6113c8799937fbcc3
tristaaa <tristaaa56@126.com>
2019-12-06 16:54:07 -0800

    demo

---
+++ trendyou/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  title = 'Strategies Behind Youtube Trending Videos';
+  public isActive = false;
+
+  @HostListener("window:scroll",['$event']) changeNav($event){
+    var currTop = $event.srcElement.scrollingElement.scrollTop
+    if (currTop >30) {
+      this.isActive = true;
+    }else{
+      this.isActive = false;
+    }
+  }
+}
